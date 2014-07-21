c7c085bdd392bd2d0158ed791122f67ee1908885
Jasmina Kruhak <jkruhak@gmail.com>
2014-07-21 23:24:49 +0400

    Revert "ggg"
    This reverts commit 9c876a17e7f1fa5bcb40dcbc2d233bec01e714a9.

--- js/app.js
+++ js/app.js
@@ -11,20 +11,17 @@ $(document).ready(function() {
 		//product-price variables
 		var inputprice = +$('#price').find('#product-price').val();
 		var rowtotal = inputprice*inputquantity;
-		var total = rowtotal.toFixed(2);
-		var listprice = $('<li>'+total+'</li>');
-		var row = $('<li>'+inputquantity+ 'x ' +inputitem+ ' = &#36;' +total+'</li>');
+		var listprice = $('<li>'+rowtotal+'</li>');
 		if (event.which == '13') {
 			//product-quantity
-			//$('.quantity-list').append(listquantity);
+			$('.quantity-list').append(listquantity);
 			$('#product-quantity').val('1');
 			//product-name
-			//$('.product-list').append(listitem); 
+			$('.product-list').append(listitem); 
 			$('#product-name').val('');
 			//product-price
-			//$('.price-list').append(listprice);
+			$('.price-list').append(listprice);
 			$('#product-price').val('0.00');
-			$('.product-list').append(row);
 		}
 	});
 
@@ -33,40 +30,31 @@ $(document).ready(function() {
 		//product-quantity
 		var inputquantity = +$('#quantity').find('#product-quantity').val();
 		var listquantity = $('<li>'+inputquantity+'</li>');
-		//$('.quantity-list').append(listquantity);
+		$('.quantity-list').append(listquantity);
 		$('#product-quantity').val('1');
 		//product-name
 		var inputitem = $('#product').find('#product-name').val();
 		var listitem = $('<li>'+inputitem+'</li>');
-		//$('.product-list').append(listitem); 
+		$('.product-list').append(listitem); 
 		$('#product-name').val('');
 		//product-price
 		var inputprice = +$('#price').find('#product-price').val();
 		var rowtotal = inputprice*inputquantity;
-		var total = rowtotal.toFixed(2);
-		var listprice = $('<li>'+total+'</li>');
+		var listprice = $('<li>'+rowtotal+'</li>');
+		$('.price-list').append(listprice);
 		$('#product-price').val('0.00');
-		var row = $('<li>'+inputquantity+ 'x ' +inputitem+ ' = &#36;' +total+'</li>');
-		$('.product-list').append(row);
 	});
-
-	//strikethrough when item is clicked
-	$('#lists').on('click', 'li', function() {
-		$(this).toggleClass('gotit');
-	});
-
-	//delete button on mouseover
-
 
 	//count number of items in list as each added
 	
 
-	//total price
-	$('#add').on('click', function() {
-		var sum = 0;
-		$('#product-price').each(function() {
-			sum += Number($(this).val());
-		});
-		$('#price-total').text(sum);
+
+	//strikethrough when item is clicked
+	$('#lists').on('click', 'li', function() {
+		$(this).closest('#lists').find('li').toggleClass('gotit');
+		//$(this).toggleClass('gotit');
 	});
+
+	
+
 })
