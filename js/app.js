d77034144956a8da31a0d843f4c2c31639b0d890
Jasmina Kruhak <jkruhak@gmail.com>
2014-07-24 20:56:46 +0400

    Added functions

--- js/app.js
+++ js/app.js
@@ -1,72 +1,82 @@
 $(document).ready(function() {
 
-	//add #product-name only using 'enter' key
-	$('#input-fields').on('keypress', function(event) {
-		//product-quantity variables
+	function addToQuantity() {
+		var inputquantity = +$('#quantity').find('#product-quantity').val();
+		var listquantity = $('<li id="itemquantity"> <button id="remove">X</button>' + '&#32;' + '<button id="complete">V</button>' + '&#32;' +inputquantity+'</li>');
+		$('#product-quantity').val('1');
+		$('.quantity-list').append(listquantity);
+	}
+
+	function addToItem() {
+		var inputitem = $('#product').find('#product-name').val();
+		var listitem = $('<li id="itemname">'+inputitem+'</li>');
+		$('#product-name').val('');
+		$('.product-list').append(listitem);
+	}
+
+	function addToPrice() {
 		var inputquantity = +$('#quantity').find('#product-quantity').val();
 		var listquantity = $('<li>'+inputquantity+'</li>');
-		//product-name variables
-		var inputitem = $('#product').find('#product-name').val();
-		var listitem = $('<li>'+inputitem+'</li>');
-		//product-price variables
-		var inputprice = +$('#price').find('#product-price').val();
+		var inputprice = +$('#price').find('.product-price').val();
 		var rowtotal = inputprice*inputquantity;
 		var total = rowtotal.toFixed(2);
-		var listprice = $('<li>'+total+'</li>');
-		var row = $('<li>'+inputquantity+ 'x ' +inputitem+ ' = &#36;' +total+'</li>');
+		var listprice = $('<li id="itemprice">' + '&#36;' +total+'</li>');
+		$('.product-price').val('0.00');
+		$('.price-list').append(listprice);
+	}
+
+	//add to list using 'add' button
+	$('#input-fields').on('click', 'button', function() {
+		addToQuantity();
+		addToItem();
+		addToPrice();
+	});
+
+	//add to list using 'enter' key
+	$('#input-fields').on('keypress', function(event) {
 		if (event.which == '13') {
-			//product-quantity
-			//$('.quantity-list').append(listquantity);
-			$('#product-quantity').val('1');
-			//product-name
-			//$('.product-list').append(listitem); 
-			$('#product-name').val('');
-			//product-price
-			//$('.price-list').append(listprice);
-			$('#product-price').val('0.00');
-			$('.product-list').append(row);
+			addToQuantity();
+			addToItem();
+			addToPrice();
 		}
 	});
 
-	//add #product-quantity+#product-name+#product-price using 'add' button
-	$('#input-fields').on('click', 'button', function() {
-		//product-quantity
-		var inputquantity = +$('#quantity').find('#product-quantity').val();
-		var listquantity = $('<li>'+inputquantity+'</li>');
-		//$('.quantity-list').append(listquantity);
-		$('#product-quantity').val('1');
-		//product-name
-		var inputitem = $('#product').find('#product-name').val();
-		var listitem = $('<li>'+inputitem+'</li>');
-		//$('.product-list').append(listitem); 
-		$('#product-name').val('');
-		//product-price
-		var inputprice = +$('#price').find('#product-price').val();
-		var rowtotal = inputprice*inputquantity;
-		var total = rowtotal.toFixed(2);
-		var listprice = $('<li>'+total+'</li>');
-		$('#product-price').val('0.00');
-		var row = $('<li>'+inputquantity+ 'x ' +inputitem+ ' = &#36;' +total+'</li>');
-		$('.product-list').append(row);
-	});
+	function removeItem() {
+		var quantxed = $('#lists').find('#itemquantity');
+		$(quantxed).remove();
+		var xed = $('#lists').find('#itemname');
+		$(xed).remove();
+		var pricexed = $('#lists').find('#itemprice');
+		$(pricexed).remove();
+	}
+
+	//remove item when 'X' button clicked
+	$('#lists').on('click', '#remove', removeItem); 
 
 	//strikethrough when item is clicked
-	$('#lists').on('click', 'li', function() {
-		$(this).toggleClass('gotit');
-	});
+	function completedItem() {
+		var quantxed = $('#lists').find('#itemquantity');
+		$(quantxed).toggleClass('gotit');
+		var xed = $('#lists').find('#itemname');
+		$(xed).toggleClass('gotit');
+		var pricexed = $('#lists').find('#itemprice');
+		$(pricexed).toggleClass('gotit');
+	}
 
-	//delete button on mouseover
-
+	$('#lists').on('click', '#complete', completedItem);
 
 	//count number of items in list as each added
-	
+	 	
 
-	//total price
-	$('#add').on('click', function() {
-		var sum = 0;
-		$('#product-price').each(function() {
-			sum += Number($(this).val());
-		});
-		$('#price-total').text(sum);
+	//Instructions in header
+	$('header').on('mouseover', function() {
+		$('#title').hide();
+		$('#instructions').show();
 	});
+
+	$('header').on('mouseleave', function() {
+		$('#instructions').hide();
+		$('#title').show();
+	});	
+
 })
